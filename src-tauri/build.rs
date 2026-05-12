dae4729baaf53869c4014cbd1c8875803469c662
jeyasankar <jeyasankar@users.noreply.github.com>
2026-05-12 08:46:06 -0700

    Cfg if use for mas-build feature

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -1,9 +1,9 @@
 fn main() {
-  // This was added so that editor makes available code mas-build feature available for navigating
-  println!("cargo::rustc-check-cfg=cfg(rust_analyzer)");
-
+  
   #[cfg(target_os = "macos")]
   {
+    // This was added so that editor makes available code mas-build feature available for navigating
+    println!("cargo::rustc-check-cfg=cfg(rust_analyzer)");
     use swift_rs::SwiftLinker;
     SwiftLinker::new("10.15")
       .with_ios("11")
