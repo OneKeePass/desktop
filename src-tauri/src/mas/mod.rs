b717feefecea2e52252b4a963a0c8b4ba2b2af47
jeyasankar <jeyasankar@users.noreply.github.com>
2026-05-12 08:46:06 -0700

    Cfg if use for mas-build feature

--- src-tauri/src/mas/mod.rs
+++ src-tauri/src/mas/mod.rs
@@ -1,3 +1,44 @@
+
+// To make all code for the feature "mas-build" active in VS code, 
+// Use #[cfg(any(feature = "mas-build", rust_analyzer))]
+// Also need to ensure that rust_analyzer cfg is used in  src-tauri/build.rs
+
+cfg_if::cfg_if! {
+    if #[cfg(any(feature = "mas-build", rust_analyzer))] {
+        pub(crate) mod bookmarks;
+        mod real;
+        pub(crate) use real::*;
+    } else {
+        mod noop;
+        pub(crate) use noop::*;
+    }
+}
+
+// Previously used
+
+// Active for the cases when we do not use "mas-build" feature so that the code is grayed out for that
+/*
+#[cfg(any(feature = "mas-build"))]
+pub(crate) mod bookmarks;
+
+#[cfg(any(feature = "mas-build"))]
+mod real;
+
+#[cfg(not(any(feature = "mas-build")))]
+mod noop;
+
+#[cfg(not(any(feature = "mas-build")))]
+pub(crate) use noop::*;
+
+#[cfg(any(feature = "mas-build"))]
+pub(crate) use real::*;
+*/
+
+// To make all code for the feature "mas-build" active in VS code, we need to do the following
+// Uncomment the following. Comment the above
+// Also need to ensure that rust_analyzer cfg is used in  src-tauri/build.rs
+
+/* 
 #[cfg(any(feature = "mas-build", rust_analyzer))]
 pub(crate) mod bookmarks;
 
@@ -12,3 +53,4 @@ pub(crate) use noop::*;
 
 #[cfg(any(feature = "mas-build", rust_analyzer))]
 pub(crate) use real::*;
+*/
